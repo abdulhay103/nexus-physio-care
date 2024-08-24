7edab97b292434d475a3a57a1244e3cc376b8606
abdulhay103 <abdulhay.me@gmail.com>
2024-08-24 19:01:58 +0600

    initial setup

---
+++ src/app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className=" flex flex-col justify-center items-center min-h-screen">
+      <h1>Welcome to Nexus Physio Care</h1>
+      <p>Any Query: Pls call- 01517813516, 01557453683</p>
+    </main>
+  );
+}
