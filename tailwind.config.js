9099e4e21d275beafd191cf1eeb0be2084a6a5c8
abdulhay103 <abdulhay.me@gmail.com>
2024-09-19 02:58:18 +0600

    working good

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,50 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    // colors: {
+    //   primary: "#107030",
+    //   secondary: "#40BF6A",
+    //   deepBlue: "#2D64B9",
+    //   lightBlue: "#13C8EC",
+    //   amber: " #FFFF00",
+    //   lightCtreuse: "#DCFF64",
+    //   deepKhaki: "#405000",
+    //   gray: "#616161",
+    //   lightGray: "#acacac",
+    //   white: "#FFFFFF",
+    //   whiteSmoke: "#f8f6f3",
+    //   black: "#000000",
+    //   red: "#F33725",
+    //   purple: "#7D3C98",
+    //   orange: "#FF5733",
+    // },
+    fontFamily: {
+      Kanit: ["Kanit", "sans-serif"],
+      Dancing: ["Dancing Script", "cursive"],
+      Rubik: ["Rubik", "sans-serif"],
+    },
+    screens: {
+      esm: "480px",
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      "2xl": "1800px",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "1rem",
+        md: "2rem",
+        lg: "2rem",
+        xl: "2rem",
+        "2xl": "9rem",
       },
     },
   },
