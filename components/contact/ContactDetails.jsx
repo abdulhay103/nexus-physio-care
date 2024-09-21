b15ffeb5fad699be4c23e8856946219f62c570dc
abdulhay103 <abdulhay.me@gmail.com>
2024-09-22 01:32:53 +0600

    working with contact details

--- components/contact/ContactDetails.jsx
+++ components/contact/ContactDetails.jsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
-import { FaEnvelope, FaEnvelopeOpenText, FaWhatsapp } from "react-icons/fa";
+import { FaEnvelopeOpenText, FaWhatsapp } from "react-icons/fa";
 import { FiPhoneIncoming } from "react-icons/fi";
 import { MdOutlineConnectWithoutContact } from "react-icons/md";
+import { BsCalendar2Check } from "react-icons/bs";
+import { FaLocationPinLock } from "react-icons/fa6";
 
 export default function ContactDetails() {
   return (
@@ -11,7 +13,7 @@ export default function ContactDetails() {
         <div className="hover-500 col-span-12 rounded-l-3xl bg-cyan-700 p-6 text-center text-white lg:col-span-4">
           <div className="flex justify-center">
             <div className="flex items-center gap-3">
-              <MdOutlineConnectWithoutContact className="h-12 w-12 lg:h-16 lg:w-16" />
+              <MdOutlineConnectWithoutContact className="h-12 w-12 lg:h-12 lg:w-12" />
               <h2 className="font-h2 underline underline-offset-[6px]">
                 দ্রুত যোগাযোগঃ
               </h2>
@@ -22,7 +24,7 @@ export default function ContactDetails() {
             কাঙ্খিত পরিষেবা নিন।
           </p>
 
-          <div className="flex items-center overflow-hidden rounded border bg-white">
+          <div className="mt-5 flex items-center overflow-hidden rounded border bg-white">
             <div className="w-16 pl-3 text-orange-700">
               <FiPhoneIncoming className="h-7 w-7 font-bold lg:h-7 lg:w-7" />
               <FaWhatsapp className="h-7 w-7 font-bold lg:h-7 lg:w-7" />
@@ -52,16 +54,15 @@ export default function ContactDetails() {
         </div>
         <div className="hover-500 col-span-12 bg-orange-700 p-6 text-center text-white lg:col-span-4">
           <div className="flex justify-center">
-            <div className="flex items-center gap-3">
-              <MdOutlineConnectWithoutContact className="h-12 w-12 lg:h-16 lg:w-16" />
+            <div className="flex items-center gap-3 lg:gap-5">
+              <BsCalendar2Check className="h-12 w-12 lg:h-12 lg:w-12" />
               <h2 className="font-h2 underline underline-offset-[6px]">
-                দ্রুত যোগাযোগঃ
+                এপয়নমেন্ট দিন
               </h2>
             </div>
           </div>
           <p className="py-5">
-            ফোন অথবা হোয়াটস্যাপ এ যোগাযোগে খুবই দ্রুত এবং বিষয় ভিত্তিক আলোচনায়
-            কাঙ্খিত পরিষেবা নিন।
+            আপনার চিকিৎসকের সাথে সুবিধাজনক সময়ে এপয়নমেন্ট বুক করুন।
           </p>
 
           <div className="flex items-center overflow-hidden rounded border bg-white">
@@ -95,15 +96,15 @@ export default function ContactDetails() {
         <div className="hover-500 col-span-12 rounded-r-3xl bg-green-700 p-6 text-center text-white lg:col-span-4">
           <div className="flex justify-center">
             <div className="flex items-center gap-3">
-              <MdOutlineConnectWithoutContact className="h-12 w-12 lg:h-16 lg:w-16" />
+              <FaLocationPinLock className="h-12 w-12 lg:h-12 lg:w-12" />
               <h2 className="font-h2 underline underline-offset-[6px]">
-                দ্রুত যোগাযোগঃ
+                আমাদের ঠিকানাঃ
               </h2>
             </div>
           </div>
           <p className="py-5">
-            ফোন অথবা হোয়াটস্যাপ এ যোগাযোগে খুবই দ্রুত এবং বিষয় ভিত্তিক আলোচনায়
-            কাঙ্খিত পরিষেবা নিন।
+            চিকিৎসা সেবা ব্যতীত অন্যান্য দাপ্তরিক প্রয়োজনে আপনি আমাদের অফিসে
+            সরাসরি যোগাযোগ করুন।
           </p>
 
           <div className="flex items-center overflow-hidden rounded border bg-white">
