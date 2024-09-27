fca83f8ec400b890d9366d243b5eb8f8dfc55a4d
abdulhay103 <abdulhay.me@gmail.com>
2024-09-28 01:19:54 +0600

    working well

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,14 +1,18 @@
 import AllPosts from "@/components/blog/AllPosts";
 import BottomPost from "@/components/blog/BottomPost";
 import Top5RecentPost from "@/components/blog/Top5RecentPost";
-import Link from "next/link";
+import BlogBreadCrumb from "@/components/breadcrumb/BlogBreadCrumb";
+import { Fragment } from "react";
 
 export default function ContactPage() {
   return (
-    <main className="section-padding-minimal bg-gradient-to-t from-transparent via-cyan-50 to-transparent">
-      <Top5RecentPost />
-      <AllPosts />
-      <BottomPost />
-    </main>
+    <Fragment>
+      <BlogBreadCrumb text="ব্লগ পড়ুন, নতুন কিছু জানুন" pathNames="/blog" />
+      <main className="section-padding-minimal bg-gradient-to-t from-transparent via-cyan-50 to-transparent">
+        <Top5RecentPost />
+        <AllPosts />
+        <BottomPost />
+      </main>
+    </Fragment>
   );
 }
