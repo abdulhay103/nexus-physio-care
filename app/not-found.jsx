a2f877cdb3ee12e725223be1398dc8a77dfe85eb
abdulhay103 <abdulhay.me@gmail.com>
2024-09-27 13:07:39 +0600

    working good

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,31 +1,27 @@
 import Link from "next/link";
+import errorImg from "../public/others/error.png";
+import Image from "next/image";
+import { PiKeyReturn } from "react-icons/pi";
 
-// Server-side data fetching function
-export async function getServerSideProps(context) {
-  const { req } = context;
-  const domain = req.headers.host; // Get the host from the request headers
-  console.log(domain);
-
-  // Assuming `getSiteData` is a function that fetches data using the domain
-  const data = await getSiteData(domain);
-
-  return {
-    props: {
-      data,
-    },
-  };
-}
-
-export default function NotFound({ data }) {
-  console.log(data);
-
+export default function NotFound() {
   return (
     <div className="flex min-h-screen w-full items-center justify-center">
-      <h2>Not Found: </h2>
-      <p>Could not find requested resource</p>
-      <p>
-        View <Link href="/blog">all posts</Link>
-      </p>
+      <div className="w-full lg:w-1/3">
+        <div className="relative h-96 w-full lg:h-[420px] 2xl:h-[520px]">
+          <Image
+            src={errorImg}
+            fill
+            className="object-cover"
+            alt="error-Image"
+          />
+        </div>
+        <div className="hover-300 flex items-center justify-center gap-2 font-safodi text-[#407aff] hover:text-orange-600 2xl:gap-3 2xl:pt-5">
+          <PiKeyReturn className="h-6 w-6" />
+          <Link href="/" className="text-xl underline">
+            Home
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
